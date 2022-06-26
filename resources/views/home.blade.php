330fc0ef44ea3791e3eb4cd49879aced20f4d83e
bodascaluca <bodasca.luca@gmail.com>
2022-06-27 00:49:05 +0200

    add card-list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,25 @@
 
 @section('main_content')
     <section class="section-bg">
-        <div class="container_uno">
-            <div class="card">
-                <div class="comics-card">
-                @foreach ($comics_array as $comics)
+        <div class="container">
+
+            <div class="comics-list">
+
+                <ul class="comics-card">
+                 @foreach ($comics_array as $comics)
+                 <li class="card">
                     <img src="{{ $comics['thumb'] }}" alt="">
-                    <h4> {{  $comics['title']  }} </h4>
-                @endforeach
-                </div>
+                    <h4> {{  $comics['title'] }} </h4>
+                 </li>
+                 @endforeach
+                </ul>
+
             </div>
+
+            <div class="targhetta">
+                <h3>CURRENT SERIES</h3>
+            </div>
+
         </div>
     </section>
 @endsection
